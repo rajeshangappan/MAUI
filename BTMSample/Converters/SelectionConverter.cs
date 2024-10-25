0503002cfbc3cedbd509b7a29fb8e1b6f7ba59c0
rajesh <ar@sabic.com>
2024-10-25 12:09:56 +0530

    Updated the sample

---
+++ BTMSample/Converters/SelectionConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace BTMSample
+{
+    public class SelectionConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            // Convert the bool value to a Color
+            bool boolValue = (bool)value;
+            return boolValue ? Colors.BlueViolet : Colors.LightGray;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            //throw new NotImplementedException();
+            return null;
+        }
+    }
+}
